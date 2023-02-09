c989677a341fa761fe0eb28afe8348d90d531c4e
Amin <a.aminian321@gmail.com>
2023-02-09 18:41:38 +0330

    app_settings.py added

---
+++ zero_migrations/app_settings.py
@@ -0,0 +1,10 @@
+IGNORE_APPS = {
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "zero_migrations"
+}
